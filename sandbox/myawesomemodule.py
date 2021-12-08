938922afa683e4ea718a9cd44544ad6b11ba329d
Daiji Fukagawa <dfukagaw28@gmail.com>
2021-12-08 11:06:40 +0900

    Add HW10

---
+++ sandbox/myawesomemodule.py
@@ -0,0 +1,5 @@
+def factorial(n):
+    ans = 1
+    for k in range(1, n + 1):
+        ans *= k
+    return ans
---
+++ sandbox/testmodule.py
@@ -0,0 +1,18 @@
+# 標準ライブラリをインポートする
+import sys
+
+# 変数を定義する
+a = 1234
+b = "Hello"
+
+# 関数を定義する
+def foo():
+    print("This is the function 'foo'")
+
+# 文字列を表示する
+print("AAAAA")
+
+print(__name__)
+
+if __name__ == '__main__':
+    print("BBBBB")
